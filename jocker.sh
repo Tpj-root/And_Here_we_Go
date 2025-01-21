625afdd543f928ee3b1b87aa1ea4280b72b63585
sab <sab@debian.sab>
2025-01-21 13:57:46 +0530

    And Here We Go

--- jocker.sh
+++ jocker.sh
@@ -1,49 +1,3 @@
-# And_Here_we_Go
-I'm not stupid. I'm just too lazy to show how smart I am.
-
-
-
-
-```
-https://www.youtube.com/watch?v=24cQfQJ79Rs
-```
-
-![](joker.gif)
-
-
-In The Dark Knight (2008), the phrase "And here we go" is spoken by the Joker, played by Heath Ledger. 
-It occurs in a pivotal moment during the movie when the Joker is about to carry out a dangerous and chaotic plan, further escalating the conflict between him and Batman.
-
-
-### Context:
-
-The scene takes place after the Joker has caused chaos and confusion in Gotham City.
-He is prepared to **push things** further by manipulating situations to create fear, confusion, and destruction.
-When the Joker says "And here we go," it marks the beginning of an intense, unpredictable phase in his plan, and signifies his enjoyment in the chaos he is about to create.
-
-
-**Psychological games:**
-
-The Joker uses this phrase to signal that his next move, which may seem irrational or dangerous, is part of his broader goal of creating disorder and testing people's morality. It's about pushing Batman and others to the limits of their tolerance and sanity.
-
-**Anticipation of chaos:**
-
-The Joker thrives on creating mayhem, and "And here we go" is his way of expressing excitement for the chaos he’s about to unleash. It shows his lack of concern for the consequences and his complete embrace of anarchy.
-
-
-
-
-**My push things:**
-
-Create a function for a quick way to commit and push changes using a single command.
-
-```
-gitHereWeGo
-```
-
-Copy the script below and paste it into your .bashrc file:
-
-```
 print_phrase() {
     figlet "And Here We Go"
 }
@@ -84,15 +38,10 @@ function gitHereWeGo() {
     fi
 }
 
+alias githerewego='gitHereWeGo'
+alias gitgo='gitHereWeGo'
+
+
 #print_phrase
 #print_face
-
-```
-
-
-
-
-
-
-
 
